695cabb649e6a24938648f14c004896e63051c15
maxime.cognie <maxime.cognie@protonmail.com>
2024-06-07 06:44:30 +0000

    add nav2 algorithm for simulated robot

--- path_planner_server/launch/pathplanner.launch.py
+++ path_planner_server/launch/pathplanner.launch.py
@@ -8,19 +8,30 @@ from launch.substitutions import LaunchConfiguration
 
 def launch_setup(context, *args, **kwargs):
     sim = context.perform_substitution(LaunchConfiguration('sim'))
+    filters = context.perform_substitution(LaunchConfiguration('filters'))
 
     pkg_share_dir = os.path.join(get_package_share_directory('path_planner_server'), 'config')
+    filter_config_dir = os.path.join(pkg_share_dir, 'keepout_filter')
 
     if(sim == 'True'):
-        nav2_yaml = os.path.join(pkg_share_dir, 'planner_server.yaml')
-        controller_yaml = os.path.join(pkg_share_dir, 'controller.yaml')
+        config_dir = pkg_share_dir
+        filters_yaml = ''
+        if(filters == 'True'):
+            config_dir = filter_config_dir
+            filters_yaml = os.path.join(config_dir, 'filters.yaml')
+        nav2_yaml = os.path.join(config_dir, 'planner_server.yaml')
+        controller_yaml = os.path.join(config_dir, 'controller.yaml')
         bt_navigator_yaml = os.path.join(pkg_share_dir, 'bt.yaml')
         recovery_yaml = os.path.join(pkg_share_dir, 'recovery.yaml')
         cmd_vel_topic = 'diffbot_base_controller/cmd_vel_unstamped'
         use_sim_time = True
     else:
-        nav2_yaml = os.path.join(pkg_share_dir, 'planner_server_real.yaml')
-        controller_yaml = os.path.join(pkg_share_dir, 'controller_real.yaml')
+        config_dir = pkg_share_dir
+        filters_yaml = ''
+        if(filters == 'True'):
+            config_dir = filter_config_dir
+        nav2_yaml = os.path.join(config_dir, 'planner_server_real.yaml')
+        controller_yaml = os.path.join(config_dir, 'controller_real.yaml')
         bt_navigator_yaml = os.path.join(pkg_share_dir, 'bt_real.yaml')
         recovery_yaml = os.path.join(pkg_share_dir, 'recovery_real.yaml')
         cmd_vel_topic = 'cmd_vel'
@@ -58,6 +69,32 @@ def launch_setup(context, *args, **kwargs):
         output='screen'
         )
     
+    filter_mask_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='filter_mask_server',
+        output='screen',
+        emulate_tty=True,
+        parameters=[filters_yaml])
+
+    costmap_filter_info_node = Node(
+        package='nav2_map_server',
+        executable='costmap_filter_info_server',
+        name='costmap_filter_info_server',
+        output='screen',
+        emulate_tty=True,
+        parameters=[filters_yaml])
+    
+    nav2_nodes = ['planner_server', 'controller_server', 'behavior_server', 'bt_navigator']
+    if (filters == 'True'):
+        nav2_nodes = [
+        'planner_server', 
+        'controller_server', 
+        'behavior_server', 
+        'bt_navigator', 
+        'filter_mask_server',
+        'costmap_filter_info_server']
+    
     nav2_lifecycle_manager = Node(
         package='nav2_lifecycle_manager',
         executable='lifecycle_manager',
@@ -65,7 +102,7 @@ def launch_setup(context, *args, **kwargs):
         output='screen',
         parameters=[{'use_sim_time': use_sim_time},
                     {'autostart': True},
-                    {'node_names': ['planner_server', 'controller_server', 'behavior_server', 'bt_navigator']}]
+                    {'node_names': nav2_nodes}]
         )
     
     rviz_config_file = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'pathplanning.rviz')
@@ -73,7 +110,7 @@ def launch_setup(context, *args, **kwargs):
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        output='screen',
+        output='log',
         arguments=['-d', rviz_config_file],
         parameters=[{'use_sim_time': use_sim_time}])
 
@@ -85,6 +122,17 @@ def launch_setup(context, *args, **kwargs):
         nav2_lifecycle_manager,
         rviz
     ]
+    if(filters == 'True'):
+        nodes_to_start = [
+            planner_node,
+            controller_node,
+            bt_navigator_node,
+            recovery_node,
+            filter_mask_node,
+            costmap_filter_info_node,
+            nav2_lifecycle_manager,
+            rviz
+        ]
 
     return nodes_to_start
 
@@ -93,7 +141,12 @@ def generate_launch_description():
         default_value='True', 
         description='Boolean argument to specify if the robot is simulated or not.\nTrue if the robot is simulated and False if working with a real robot.')
 
+    filter_arg = DeclareLaunchArgument('filters',
+        default_value='False',
+        description='Boolean argument.\nSet to True to activate the costmaps filters.')
+
     return LaunchDescription([
         sim_arg,
+        filter_arg,
         OpaqueFunction(function=launch_setup)
     ])
